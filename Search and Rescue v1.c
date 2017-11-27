8ff5aaa1167b47f9de82964c30fde22af63122b9
Ben <coolmisterbb22@aol.com>
2017-11-27 08:39:38 -0800

    initial notes and setup

---
+++ Search and Rescue v1.c
@@ -0,0 +1,51 @@
+
+void gState0()
+{
+}
+
+void gState1()
+{
+}
+
+void gState2()
+{
+}
+
+void gState3()
+{
+}
+
+task main()
+{
+	int gState = 0;
+
+	//use methods for each gState
+
+	while(gState == 0)
+	{
+		//position robot to travel to block a (assuming extra 4 pts for inital position)
+	}
+
+	while(gState == 1)
+	{
+		//travel to block a
+		//retrieve block a
+	}
+
+	while(gState == 2)
+	{
+		//travel to block f
+		//retrieve block f
+	}
+
+	while(gState == 3)
+	{
+		//return blacok f
+	}
+
+	while(gState == 4)
+	{
+		//game end
+	}
+
+}
